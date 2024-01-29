4e4e92e78c38fed5f9da9c00886a4a38d8ae14a4
islam <islam.kutkeev@gmail.com>
2024-01-30 00:57:09 +0600

    feat: file manager service implemented

--- Service/DocumentationGenerator.Service/Services/FileManagerService/CustomFileManagerService.cs
+++ Service/DocumentationGenerator.Service/Services/FileManagerService/CustomFileManagerService.cs
@@ -1,5 +1,145 @@
+using AutoMapper;
+using DocumentationGenerator.Service.Constants;
+using DocumentationGenerator.Service.Dtos;
+using DocumentationGenerator.Service.Entities;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.EntityFrameworkCore;
+using System.Web;
+
 namespace DocumentationGenerator.Service.Services.FileManagerService;
 
 public class CustomFileManagerService : IFileManagerService
 {
+    private ILogger<CustomFileManagerService> _logger;
+    private readonly DatabaseContext _dbContext;
+    private readonly IConfiguration _configuration;
+    private readonly IMapper _mapper;
+
+
+    public CustomFileManagerService(ILogger<CustomFileManagerService> logger, DatabaseContext dbContext, IConfiguration configuration, IMapper mapper)
+    {
+        _logger = logger;
+        _dbContext = dbContext;
+        _configuration = configuration;
+        _mapper = mapper;
+    }
+
+    public async Task<List<FileDto>> GetAllFilesAsync()
+    {
+        var files = await _dbContext.Files.ToListAsync();
+        return _mapper.Map<List<FileDto>>(files);
+    }
+
+    public async Task<FileDto> SaveFileAsync(IFormFile file)
+    {
+        if (file == null || file.Length == 0)
+        {
+            throw new ArgumentNullException();
+        }
+
+        // Ensure the "uploads" directory exists
+        var uploadsDir = _configuration["UploadDirectory"];
+        if (!Directory.Exists(uploadsDir))
+        {
+            Directory.CreateDirectory(uploadsDir);
+        }
+
+        string fileExtension = Path.GetExtension(file.FileName);
+
+        // Check file's extension
+        if (!fileExtension.Equals(".html"))
+        {
+            throw new FormatException();
+        }
+
+        var identifier = Guid.NewGuid();
+        string fileName = Path.GetFileNameWithoutExtension(file.FileName);
+        string uniqueFileName = $"{identifier}{fileExtension}";
+
+        // Combine the uploads folder with the unique filename
+        var filePath = Path.Combine(uploadsDir, uniqueFileName);
+
+        // Save the file to the server
+        using (var stream = new FileStream(filePath, FileMode.Create))
+        {
+            await file.CopyToAsync(stream);
+        }
+
+        // Save file info to database
+        var fileDetails = new FileEntity()
+        {
+            Id = identifier,
+            Name = fileName,
+            SavedPath = uploadsDir,
+            Status = FileStatus.New
+        };
+
+        await _dbContext.Files.AddAsync(fileDetails);
+        await _dbContext.SaveChangesAsync();
+
+        return _mapper.Map<FileDto>(fileDetails);
+    }
+
+    public async Task DeleteFileAsync(string id)
+    {
+        // Find file info at database
+        var fileDetail = await _dbContext.Files.FirstOrDefaultAsync(x => x.Id.ToString().Equals(id));
+
+        if (fileDetail == null)
+        {
+            _logger.LogWarning("No files found at database with the id: {id}", id);
+            return;
+        }
+
+        // Find all files on the service
+        var directoryInfo = new DirectoryInfo(fileDetail.SavedPath!);
+        var matchingFiles = directoryInfo.GetFiles($"{fileDetail.Id}.*");
+
+        if (matchingFiles.Length == 0)
+        {
+            _logger.LogWarning("No files found at host with the name: {id}", id);
+            return;
+        }
+
+        // Delete every existing file
+        foreach (var file in matchingFiles)
+        {
+            System.IO.File.Delete(file.FullName);
+        }
+
+        _dbContext.Files.Remove(fileDetail);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task<FileDto?> ObtainFileAsync(string id, string extension)
+    {
+        // Find file info at database
+        var fileDetail = await _dbContext.Files.FirstOrDefaultAsync(x => x.Id.ToString().Equals(id));
+
+        if (fileDetail == null)
+        {
+            _logger.LogWarning("No files found at database with the id: {id}", id);
+            return null;
+        }
+
+        string filePath = Path.Combine(fileDetail.SavedPath, $"{fileDetail.Id}{extension}");
+
+        if (!File.Exists(filePath))
+        {
+            _logger.LogWarning("No files found with specified extension: {ext}", extension);
+            return null;
+        }
+
+        var provider = new FileExtensionContentTypeProvider();
+
+        var result = new FileDto();
+        result.Name = $"{fileDetail.Name}{extension}";
+        result.Content = await File.ReadAllBytesAsync(filePath);
+        if (provider.TryGetContentType(filePath, out string? contentType))
+        {
+            result.ContentType = contentType;
+        }
+
+        return result;
+    }
 }
--- Service/DocumentationGenerator.Service/Services/FileManagerService/IFileManagerService.cs
+++ Service/DocumentationGenerator.Service/Services/FileManagerService/IFileManagerService.cs
@@ -1,5 +1,33 @@
+using DocumentationGenerator.Service.Dtos;
+
 namespace DocumentationGenerator.Service.Services.FileManagerService;
 
 public interface IFileManagerService
 {
+    /// <summary>
+    /// Obtains all uploaded files
+    /// </summary>
+    /// <returns>List of <see cref="FileDto"/></returns>
+    Task<List<FileDto>> GetAllFilesAsync();
+
+    /// <summary>
+    /// Saves new html file on server
+    /// </summary>
+    /// <param name="file">Uploaded file</param>
+    /// <returns>Saved file information <see cref="FileDto"/></returns>
+    Task<FileDto> SaveFileAsync(IFormFile file);
+
+    /// <summary>
+    /// Deletes all information about file and all files from server
+    /// </summary>
+    /// <param name="id">File's identifier</param>
+    Task DeleteFileAsync(string id);
+
+    /// <summary>
+    /// Obtains file's content and metadata
+    /// </summary>
+    /// <param name="id">File's identifier</param>
+    /// <param name="extension">File's extension</param>
+    /// <returns>Metadata of file <see cref="FileDto?"/></returns>
+    Task<FileDto?> ObtainFileAsync(string id, string extension);
 }
